92b708df2c08061d0f856d6c06e1128be03ceb0c
WasabiRaptor <ketchupraptor@gmail.com>
2018-11-02 20:44:02 -0400

    make roofs not do anything

--- data/maps/roofs.asm
+++ data/maps/roofs.asm
@@ -9,34 +9,34 @@
 MapGroupRoofs:
 ; entries correspond to map groups
 ; values are indexes for Roofs (see below)
-	db -1             ;  0
-	db ROOF_OLIVINE   ;  1 (Olivine)
-	db ROOF_AZALEA    ;  2 (Mahogany)
-	db ROOF_NEW_BARK  ;  3
-	db ROOF_VIOLET    ;  4 (Ecruteak)
-	db ROOF_AZALEA    ;  5 (Blackthorn)
-	db ROOF_NEW_BARK  ;  6
-	db ROOF_NEW_BARK  ;  7
-	db ROOF_AZALEA    ;  8 (Azalea)
-	db ROOF_AZALEA    ;  9 (Lake of Rage)
-	db ROOF_VIOLET    ; 10 (Violet)
-	db ROOF_GOLDENROD ; 11 (Goldenrod)
-	db ROOF_NEW_BARK  ; 12
-	db -1             ; 13
-	db -1             ; 14
-	db -1             ; 15
-	db -1             ; 16
-	db -1             ; 17
-	db -1             ; 18
-	db ROOF_NEW_BARK  ; 19 (Silver Cave)
-	db -1             ; 20
-	db -1             ; 21
-	db ROOF_OLIVINE   ; 22 (Cianwood)
-	db -1             ; 23
-	db ROOF_NEW_BARK  ; 24 (New Bark)
-	db -1             ; 25
-	db ROOF_NEW_BARK  ; 26 (Cherrygrove)
-	db ROOF_NEW_BARK  ; 27 (dungeon)
+	db -1	;  0
+	db -1	;  1 (Olivine)
+	db -1	;  2 (Mahogany)
+	db -1	;  3
+	db -1	;  4 (Ecruteak)
+	db -1	;  5 (Blackthorn)
+	db -1	;  6
+	db -1	;  7
+	db -1	;  8 (Azalea)
+	db -1	;  9 (Lake of Rage)
+	db -1	; 10 (Violet)
+	db -1	; 11 (Goldenrod)
+	db -1	; 12
+	db -1	; 13
+	db -1	; 14
+	db -1	; 15
+	db -1	; 16
+	db -1	; 17
+	db -1	; 18
+	db -1	; 19 (Silver Cave)
+	db -1	; 20
+	db -1	; 21
+	db -1	; 22 (Cianwood)
+	db -1	; 23
+	db -1	; 24 (New Bark)
+	db -1	; 25
+	db -1	; 26 (Cherrygrove)
+	db -1	; 27 (dungeon)
 
 Roofs:
 ; entries correspond to ROOF_* constants
