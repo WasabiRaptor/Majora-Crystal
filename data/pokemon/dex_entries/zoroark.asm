142f6182256f068ed4649580aeeb11802618fef0
WasabiRaptor <ketchupraptor@gmail.com>
2019-02-20 03:10:01 -0500

    THE POKEDEX IS DONE

--- data/pokemon/dex_entries/zoroark.asm
+++ data/pokemon/dex_entries/zoroark.asm
@@ -1,4 +1,10 @@
 	db "ILLUSION FOX@" ; species name
 	dw 503, 1788 ; height, weight
 
-	db   ""
+    db   "Zoroark can create"
+    next "illusions to fool"
+    next "people and drive"
+
+    page "them away from"
+    next "their dens.@"
+   
