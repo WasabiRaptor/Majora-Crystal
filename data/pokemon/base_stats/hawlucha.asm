4da817548c1bfb85d96dea63907a77dbb4ab9928
WasabiRaptor <ketchupraptor@gmail.com>
2019-02-21 14:26:45 -0500

    more pokemon

--- data/pokemon/base_stats/hawlucha.asm
+++ data/pokemon/base_stats/hawlucha.asm
@@ -1,20 +1,20 @@
-	db SHROOMISH 
+	db HAWLUCHA 
 
-	db  60,  40,  60,  35,  40,  60
-    evs  1,   0,   0,   0,   0,   0
+	db  78,  92,  75, 118,  74,  63
+    evs  0,   2,   0,   0,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db GRASS, GRASS ; type
-	db 255 ; catch rate
-	db 59 ; base exp
+	db FIGHTING, FLYING ; type
+	db 100 ; catch rate
+	db 175 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
-	db 15 ; step cycles to hatch
-	INCBIN "gfx/pokemon/shroomish/front.dimensions"
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/hawlucha/front.dimensions"
 	db 0, 0 ;form differences
 	db 0, 0	;abilities
-	db GROWTH_FLUCTUATING ; growth rate
-	dn EGG_FAIRY, EGG_GRASS ; egg groups
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_HUMAN_LIKE, EGG_HUMAN_LIKE ; egg groups
 
 	; tm/hm learnset
 	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ZAP_CANNON, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, SWIFT,  REST, ATTRACT, FLAMETHROWER
--- data/pokemon/base_stats/luxio.asm
+++ data/pokemon/base_stats/luxio.asm
@@ -1,20 +1,20 @@
-	db SHROOMISH 
+	db LUXIO 
 
-	db  60,  40,  60,  35,  40,  60
-    evs  1,   0,   0,   0,   0,   0
+	db  60,  85,  49,  60,  60,  49
+    evs  0,   2,   0,   0,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db GRASS, GRASS ; type
-	db 255 ; catch rate
-	db 59 ; base exp
+	db ELECTRIC, ELECTRIC ; type
+	db 120 ; catch rate
+	db 127 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
-	db 15 ; step cycles to hatch
-	INCBIN "gfx/pokemon/shroomish/front.dimensions"
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/luxio/front.dimensions"
 	db 0, 0 ;form differences
 	db 0, 0	;abilities
-	db GROWTH_FLUCTUATING ; growth rate
-	dn EGG_FAIRY, EGG_GRASS ; egg groups
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_FIELD, EGG_FIELD ; egg groups
 
 	; tm/hm learnset
 	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ZAP_CANNON, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, SWIFT,  REST, ATTRACT, FLAMETHROWER
